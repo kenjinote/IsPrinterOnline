deec02f97ead66d77257602a4e700bf2f5e5e29c
kenjinote <kenji256@gmail.com>
2017-02-15 01:35:47 +0900

    printerInfo.Attributes & PRINTER_ATTRIBUTE_WORK_OFFLINE

--- Source.cpp
+++ Source.cpp
@@ -6,18 +6,19 @@ TCHAR szClassName[] = TEXT("Window");
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	static HWND hButton;
+	static HWND hStatic;
 	switch (msg)
 	{
 	case WM_CREATE:
-		hButton = CreateWindow(TEXT("BUTTON"), TEXT("デフォルトプリンターがオンラインかどうか調べる"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hStatic = CreateWindow(TEXT("STATIC"), 0, WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		SetTimer(hWnd, 0x1234, 500, 0);
 		break;
 	case WM_SIZE:
-		MoveWindow(hButton, 10, 10, 512, 32, TRUE);
+		MoveWindow(hStatic, 10, 10, LOWORD(lParam) - 20, HIWORD(lParam) - 20, TRUE);
 		break;
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK)
+	case WM_TIMER:
 		{
+			KillTimer(hWnd, 0x1234);
 			TCHAR szDefaultPrinter[256] = { 0 };
 			DWORD dwSize = _countof(szDefaultPrinter);
 			if (GetDefaultPrinter(szDefaultPrinter, &dwSize))
@@ -25,40 +26,26 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 				HANDLE hPrinter;
 				if (OpenPrinter(szDefaultPrinter, &hPrinter, 0) != 0)
 				{
+					const DWORD dwLevel = 2;
+					DWORD dwNeeded;
+					GetPrinter(hPrinter, dwLevel, 0, 0, &dwNeeded);
+					LPBYTE pPrinter = (LPBYTE)LocalAlloc(LPTR, dwNeeded);
+					if (GetPrinter(hPrinter, dwLevel, pPrinter, dwNeeded, &dwNeeded) != 0)
 					{
-						const DWORD dwLevel = 2;
-						DWORD dwNeeded;
-						GetPrinter(hPrinter, dwLevel, 0, 0, &dwNeeded);
-						LPBYTE pPrinter = (LPBYTE)LocalAlloc(LPTR, dwNeeded);
-						if (GetPrinter(hPrinter, dwLevel, pPrinter, dwNeeded, &dwNeeded) != 0)
-						{
-							PRINTER_INFO_2 *pInfo = (PRINTER_INFO_2 *)pPrinter;
-							TCHAR szText[1024];
-							wsprintf(szText, TEXT("プリンター名: %s\n\n状態: %s"), szDefaultPrinter, (pInfo->Status & PRINTER_ATTRIBUTE_WORK_OFFLINE) ? TEXT("オフライン") : TEXT("オンライン"));
-							LocalFree(pPrinter);
-							MessageBox(hWnd, szText, TEXT("PRINTER_INFO_2"), 0);
-						}
-					}
-					{
-						const DWORD dwLevel = 6;
-						DWORD dwNeeded;
-						GetPrinter(hPrinter, dwLevel, 0, 0, &dwNeeded);
-						LPBYTE pPrinter = (LPBYTE)LocalAlloc(LPTR, dwNeeded);
-						if (GetPrinter(hPrinter, dwLevel, pPrinter, dwNeeded, &dwNeeded) != 0)
-						{
-							PRINTER_INFO_6 *pInfo = (PRINTER_INFO_6 *)pPrinter;
-							TCHAR szText[1024];
-							wsprintf(szText, TEXT("プリンター名: %s\n\n状態: %s"), szDefaultPrinter, (pInfo->dwStatus & PRINTER_STATUS_OFFLINE) ? TEXT("オフライン") : TEXT("オンライン"));
-							LocalFree(pPrinter);
-							MessageBox(hWnd, szText, TEXT("PRINTER_INFO_6"), 0);
-						}
+						PRINTER_INFO_2 *pInfo = (PRINTER_INFO_2 *)pPrinter;
+						TCHAR szText[1024];
+						wsprintf(szText, TEXT("プリンター名: %s\n\n状態: %s"), szDefaultPrinter, (pInfo->Attributes & PRINTER_ATTRIBUTE_WORK_OFFLINE) ? TEXT("オフライン") : TEXT("オンライン"));
+						LocalFree(pPrinter);
+						SetWindowText(hStatic, szText);
 					}
 					ClosePrinter(hPrinter);
 				}
 			}
+			SetTimer(hWnd, 0x1234, 500, 0);
 		}
 		break;
 	case WM_DESTROY:
+		KillTimer(hWnd, 0x1234);
 		PostQuitMessage(0);
 		break;
 	default:
